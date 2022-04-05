18e49ce379991074c211b715b269e0d2b814b96b
polizoto <polizoto@hotmail.com>
2022-04-05 06:51:27 -0400

    added CSS rules for Work page

--- src/components/Pages/Work.js
+++ src/components/Pages/Work.js
@@ -1,19 +1,28 @@
+import alley from '../../assets/alley.png'
+import './Work.css';
 export default function Work() {
     return (
       <div>
         <h1>Work</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-          velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-          ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-          non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-          ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-          rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-          tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-          porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-          vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus.
-        </p>
+        <div class="projects">
+            <div id="aim" class="large-project">
+                <a aria-label="Accessibility presentations" href="https://daisy.org/news-events/articles/ways-people-with-print-disabilities-read-w/"><h3>Accessible Instructional Materials</h3></a>
+            </div>
+            <div class="small-project">
+            <div id="accessibility" class="project">
+                <a aria-label="Full Stack Application - Tech Blog" href="https://immense-badlands-14812.herokuapp.com/"><h3>Tech Development Scoop App</h3></a>
+            </div>
+            <div id="bash" class="project">
+                <a aria-label="Bash Script projects" href="https://github.com/polizoto/DOCX-HTML"><h3>Bash Scripting</h3></a>
+            </div>
+            <div id="java" class="project">
+                <a aria-label="JavaScript examples" href="https://polizoto.github.io/find-an-author/"><h3>Find An Author - JavaScript</h3></a>
+            </div>
+            <div id="html" class="project">
+                <a aria-label="Full-stack App - Prost" href="https://fast-woodland-63027.herokuapp.com/"><h3>Alley Twenty-Six Bar - Menu</h3></a>
+            </div>
+            </div>
+        </div>  
       </div>
     );
   }
