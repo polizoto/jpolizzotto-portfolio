227078e7c624814e86b4877ec9db830342d23479
polizoto <polizoto@hotmail.com>
2022-04-04 23:45:48 -0400

    CSS changes and updated Home page

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -3,12 +3,10 @@ export default function About() {
       <div>
         <h1>About</h1>
         <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        I am an accessibility technologist with over 20 years of teaching experience.
+        </p>
+        <p>
+        I have a passion for coding and am looking forward to opportunities to be a front-end web developer.
         </p>
       </div>
     );
