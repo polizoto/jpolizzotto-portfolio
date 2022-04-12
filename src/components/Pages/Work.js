e89443868e462f46e6a4af38c0f4afe19f759565
polizoto <polizoto@hotmail.com>
2022-04-12 16:03:02 -0400

    updated Portfolio with Book Finder app

--- src/components/Pages/Work.js
+++ src/components/Pages/Work.js
@@ -16,35 +16,35 @@ export default class Work extends Component {
             <div className="small-projects">
             <div id="group-project2" tabIndex="0" className="project">
                 <div className="img__description">
-                <a className="app-info" aria-label="Full Stack Application - Tech Blog" href="https://fast-woodland-63027.herokuapp.com/"rel="noreferrer" target="_blank"><h3>Alley 26 Cocktail Reference</h3></a>
+                <a className="app-info" aria-label="Alley 26 Bar Menu" href="https://fast-woodland-63027.herokuapp.com/"rel="noreferrer" target="_blank"><h3>Alley 26 Cocktail Reference</h3></a>
                 <a className="GitHub-info" href="https://github.com/polizoto/Prost_4.0" title="GitHub" rel="noreferrer" target="_blank">
                 <Icon className="icon" icon="akar-icons:github-outline-fill" height="40" /></a>
                 </div>
             </div>
-            <div id="techscoop" tabIndex="0" className="project">
+            <div id="book-finder" tabIndex="0" className="project">
                 <div className="img__description">
-                <a className="app-info" aria-label="Full Stack Application - Tech Blog" href="https://immense-badlands-14812.herokuapp.com/"rel="noreferrer" target="_blank"><h3>Tech Development Scoop</h3></a>
-                <a className="GitHub-info" href="https://github.com/polizoto/tech-development-scoop" title="GitHub" rel="noreferrer" target="_blank">
+                <a className="app-info" aria-label="Open Source Book Finder" href="https://limitless-plateau-46390.herokuapp.com/"rel="noreferrer" target="_blank"><h3>Open - Source Book-Finder</h3></a>
+                <a className="GitHub-info" href="https://github.com/polizoto/openSource-bookFinder" title="GitHub" rel="noreferrer" target="_blank">
                 <Icon className="icon"  icon="akar-icons:github-outline-fill" height="40" /></a>
                 </div>
             </div>
             <div id="bash_script" tabIndex="0" className="project">
                 <div className="img__description">
-                <a className="app-info" aria-label="Full Stack Application - Tech Blog" href="https://www.youtube.com/watch?v=ophpILKTVNI"rel="noreferrer" target="_blank"><h3>DOCX-HTML Bash Script</h3></a>
+                <a className="app-info" aria-label="DOCX HTML Bash Script" href="https://www.youtube.com/watch?v=ophpILKTVNI"rel="noreferrer" target="_blank"><h3>DOCX-HTML Bash Script</h3></a>
                 <a className="GitHub-info" href="https://github.com/polizoto/DOCX-HTML" title="GitHub" rel="noreferrer" target="_blank">
                 <Icon className="icon"  icon="akar-icons:github-outline-fill" height="40" /></a>
                 </div>
             </div>
             <div id="manager-express" tabIndex="0" className="project">
                 <div className="img__description">
-                <a className="app-info" aria-label="Full Stack Application - Tech Blog" href="https://www.dropbox.com/s/pqkkrct2wip16op/Employeemanagerexpress_captioned.mp4?dl=0"rel="noreferrer" target="_blank"><h3>Employee Manager Express</h3></a>
+                <a className="app-info" aria-label="Employee Manager Express App" href="https://www.dropbox.com/s/pqkkrct2wip16op/Employeemanagerexpress_captioned.mp4?dl=0"rel="noreferrer" target="_blank"><h3>Employee Manager Express</h3></a>
                 <a className="GitHub-info" href="https://github.com/polizoto/employee-manager-express" title="GitHub" rel="noreferrer" target="_blank">
                 <Icon className="icon"  icon="akar-icons:github-outline-fill" height="40" /></a>
                 </div>
             </div>
             <div id="cash-flow" tabIndex="0" className="project">
                 <div className="img__description">
-                <a className="app-info" aria-label="Full Stack Application - Tech Blog" href="https://stark-bastion-02307.herokuapp.com/"rel="noreferrer" target="_blank"><h3>Cash Flow Analyzer</h3></a>
+                <a className="app-info" aria-label="Cash Flow Analyzer App" href="https://stark-bastion-02307.herokuapp.com/"rel="noreferrer" target="_blank"><h3>Cash Flow Analyzer</h3></a>
                 <a className="GitHub-info" href="https://github.com/polizoto/cash_flow_analyzer" title="GitHub" rel="noreferrer" target="_blank">
                 <Icon className="icon" icon="akar-icons:github-outline-fill" height="40" /></a>
                 </div>
